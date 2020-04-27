9aae1e14e5b1186da056b23d30c388ede5b4f016
lrondenet <lrondenet1@mail.csuchico.edu>
2020-04-27 13:03:43 -0700

    adding is_equal_one and is equal_two

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -66,11 +66,41 @@ TEST(PracticeTest, is_equal)
     ASSERT_EQ(first, thrid);
     thrid = temp;
     ASSERT_EQ(thrid, temp);
-
-    
-
 }
 
+TEST(PracticeTest, is_equalone)
+{
+    Practice obj;
+    int temp;
+    int first = 1;
+    int second = 2;
+    int thrid = 3;
+    obj.sortDescending(first, second, thrid);
+    ASSERT_LT(first, second);
+    temp = first;
+    ASSERT_EQ(temp, first);
+    first = second;
+    ASSERT_EQ(first, second);
+    second = temp;
+    ASSERT_EQ(second, temp);
+}
+
+TEST(PracticeTest, is_equaltwo)
+{
+    Practice obj;
+    int temp;
+    int first = 1;
+    int second = 2;
+    int thrid = 3;
+    obj.sortDescending(first, second, thrid);
+    ASSERT_LT(second, thrid);
+    temp = second;
+    ASSERT_EQ(temp, second);
+    second = thrid;
+    ASSERT_EQ(second, thrid);
+    thrid = temp;
+    ASSERT_EQ(thrid, temp);
+}
 
 // TEST(PracticeTest, is_equal)
 // {
